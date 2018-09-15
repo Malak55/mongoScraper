c7cb0c891cd13a06b01d9c1ee2bac683ef20d783
Matt <moldycrow55@gmail.com>
2018-09-14 22:30:20 -0500

    Add heroku

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 //connecting to MongoDB
-mongoose.connect('mongodb://heroku_jxkjhg1v:6s68tem51mlionrj2sneb7b53c@ds127988.mlab.com:27988/heroku_jxkjhg1v');
+mongoose.connect('mongodb://heroku_xw5bk4w9:6qolkjtmte8nhe8ago5l6qcs8h@ds157522.mlab.com:57522/heroku_xw5bk4w9');
 
 //mongoose.connect('mongodb://localhost/scraped_news');
 
